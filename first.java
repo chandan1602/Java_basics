1940489bedb576727494f10c609bdec8c2e4f429
chandan1602 <cbansal82@gmail.com>
2019-07-05 18:33:39 +0530

    Level 1 Basics

---
+++ first.java
@@ -0,0 +1,120 @@
+Java
+---------------
+    Java is an object-oriented programming language.
+    Java was created James Gosling.
+    Java support Unicode character code.(32 bit ..char 2 bytes)
+    Java is based on compiler as well as on interpreter.
+    Java is case-sensitive language.
+        |
+        A,a both are different.
+    Java is platform-independent language.
+    Java is open-source language.
+
+    Usages
+    ---------------
+        web application
+        android app development
+        desktop application
+        e-commerce
+        games
+
+Java software
+------------------------
+    jdk: Java development kit
+    |
+    jre
+    jvm
+    javac
+    java
+    javap
+    java-rmi
+
+    editor: notepad/notepad++/brackets
+    save: filename.java
+    compile:   javac filename.java ->.class
+    run:    java filename
+
+
+
+    source code   -> compiler ->Bytecode
+    .java           javac           .class
+
+
+    Bytecode   ->interpreter   ->machine code
+
+    .class       java            .exe
+
+
+
+    syntax
+    ---------------------
+    class ClassName
+    {
+        public static void main(String variablename[])
+        {
+                //statement
+
+        }
+
+    }
+
+
+    Rules fro class ClassName
+    --------------------------------
+        1)always start with capital letter or underscore.
+        2)never starts with number (0-9)
+        3)never uses of special characters execept underscore.
+        4)white spaces are not allowed instead we can use underscore.
+        5)Keywords are not allowed.
+
+
+    valid class Name
+    ------------------------
+        FirstDemo
+        _FirstDemo
+        First_Demo
+        First7
+        FirstDemo_
+
+    invalid class Name
+    ---------------------
+        8First
+        First Demo
+        for
+        %First
+        First@
+
+    
+    Access Specifiers
+    -------------------------
+        1)private: access within class only.
+        2)public: access anywhere in the class.
+        3)protected: access within class and inherited classes.
+
+
+    static
+    --------------
+        without creating an instance we can use it.
+
+    void 
+    --------------
+        is a return type.
+        empty 
+
+    main()
+    ------------------
+            main() method.
+            compilation always start from main()
+
+    (String variablename[]):
+    -------------------------------
+            default type of data is String.
+            used for command line arguments.
+
+
+    
+
+        
+
+
+    
